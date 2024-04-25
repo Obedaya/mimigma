ed52b80170b843300d21360f1eccb208884c38cf
Dorian Sanders <dorian.sanders@stud.th-deg.de>
2024-04-25 07:58:06 +0200

    non working DB integration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,45 @@
 from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException # Importiere Klassen aus dem FastAPI-Framework und die HTTPException-Klasse für Fehlerbehandlung.
+from sqlalchemy import create_engine, Column, Integer, String # Importiere Funktionen und Klassen aus SQLAlchemy für ORM.
+from sqlalchemy.ext.declarative import declarative_base # Importiere Funktion für Basisdeklaration von Modellen in SQLAlchemy.
+from sqlalchemy.orm import sessionmaker # Importiere die sessionmaker-Funktion, um Datenbanksitzungen zu erstellen.
 
 app = FastAPI()
 
+DATABASE_URL = "postgresql://db_user:db_pass@db/enigma_db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+Base.metadata.create_all(bind=engine)
+
+@app.on_event("startup")
+async def startup_event():
+    # Initial User Setup
+    db = SessionLocal()
+    db.add_all([
+        User(name="Alice"),
+        User(name="Bob"),
+        User(name="Mallory")
+        ])
+    db.commit()
+    db.close()
+
+@app.get("/users/")
+def read_users():
+    db = SessionLocal()
+    try:
+        users = db.query(User).all()
+        return [{"name": user.name} for user in users]
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
