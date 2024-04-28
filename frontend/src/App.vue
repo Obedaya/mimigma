0bb65f8cf14eb7ab0c592be69dc83a0b3ea8f36e
Ali Reza <ali.rajabi@stud.th-deg.de>
2024-04-28 16:09:54 +0200

    Importierung & Erstellung von Rotorpaneldatei

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <!-- Hier werden Tastatur + Leuchtbrett eingebunden -->
+    <!-- Hier werden Tastatur + Leuchtbrett + Rotorenpanel eingebunden -->
+    
     <Keyboard />
-    <Plugboard />
+    <Plugboard/>
+    <Rotorpanel/>
   </div>
 </template>
-
-
 
 <script>
 // Import von Tastatur-Komponente, welche oben genutzt wird
 import Keyboard from './components/Keyboard.vue';
 import Plugboard from './components/Plugboard.vue';
+import Rotorpanel from './components/Rotorpanel.vue';
 
 export default {
   name: 'App',
   components: {
     Keyboard,
     Plugboard,
+    Rotorpanel,
   },
 };
 </script>
