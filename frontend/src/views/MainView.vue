5d5761b92b036eeae422637af3cba165995cea64
lg06087 <laura.gnther@stud.th-deg.de>
2024-04-30 11:00:23 +0200

    added MainView.vue for improved structure

---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+      <Rotorpanel />
+      <Keyboard />
+      <Plugboard />
+      <RouterView/>
+    </div>
+  </template>
+  
+  <script>
+  import Keyboard from '../components/Keyboard.vue';
+  import Plugboard from '../components/Plugboard.vue';
+  import Rotorpanel from '../components/Rotorpanel.vue';
+  import { RouterLink, RouterView } from 'vue-router'
+  
+  export default {
+    components: {
+      Keyboard,
+      Plugboard,
+      Rotorpanel
+    }
+  };
+  </script>
+  
