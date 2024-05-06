412b7fae102307abc9d60fde381132ddf15eb753
Vivien Würth <wurthvivien@gmail.com>
2024-05-06 22:12:11 +0200

    added grid + borders

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,25 +1,67 @@
 <template>
-    <div id="app">
-      <Rotorpanel />
-      <Keyboard />
-      <Plugboard />
-      <!-- <RouterView/> -->
+  <div class="container">
+    <!--row 1-->
+    <div class="row">
+      <div class="col">
+
+        <div class="col-6" style='display: inline-block;'>USER</div>
+
+        <div class="col-6" style='display: inline-block;'>SETTINGS</div>
+
+      </div>
+      <div class="col-6">
+        <Rotorpanel />
+
+      </div>
+      <div class="col">
+      </div>
     </div>
-  </template>
-  
+    <!--row 2-->
+    <div class="row">
+      <div class="col">
+
+      </div>
+      <div class="col-6">
+        <Keyboard />
+
+      </div>
+      <div class="col">
+        <!-- HISTORY-->
+        <div class="border border-white"></div>
+      </div>
+    </div>
+    <!--row 3-->
+    <div class="row">
+      <div class="col">
+
+      </div>
+      <div class="col-6">
+        <Plugboard />
+
+      </div>
+      <div class="col">
+        <!--INPUT-->
+        <div class="border border-white"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script>
-import Keyboard from '../components/Keyboard.vue';
-import Plugboard from '../components/Plugboard.vue';
-import Rotorpanel from '../components/Rotorpanel.vue';
-import { RouterLink, RouterView } from 'vue-router'
+  import Keyboard from '../components/Keyboard.vue';
+  import Plugboard from '../components/Plugboard.vue';
+  import Rotorpanel from '../components/Rotorpanel.vue';
+  import {
+    RouterLink,
+    RouterView
+  } from 'vue-router'
 
-export default {
-  // name: 'App',
-  components: {
-    Keyboard,
-    Plugboard,
-    Rotorpanel
-  }
-};
+  export default {
+    // name: 'App',
+    components: {
+      Keyboard,
+      Plugboard,
+      Rotorpanel
+    }
+  };
 </script>
-
