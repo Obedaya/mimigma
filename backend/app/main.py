f51f43bcb694a779acbf774f945fcae0c1f11518
Paul Kronner <paul.kronner@stud.th-deg.de>
2024-05-06 16:35:34 +0200

    fixed Cross-Origin Error 401

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,8 +4,22 @@ from .models import Base, User
 from .utils import read_user_data, hash_password
 from .routes import users, login, general, items
 from .init_db import init_db 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #include routes in the app
 app.include_router(users.router)
