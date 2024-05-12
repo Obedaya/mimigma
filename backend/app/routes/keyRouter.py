bc72a60f7b5d50988b26998d8f32e80dae976481
pk30686 <paul.kronner@stud.th-deg.de>
2024-05-13 01:13:33 +0200

    Component doesn't refresh

--- backend/app/routes/keyRouter.py
+++ backend/app/routes/keyRouter.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request, WebSocket
 from typing import List
-
+import logging
 router = APIRouter()
 
 # Store all connected WebSocket clients
@@ -8,29 +8,26 @@ clients: List[WebSocket] = []
 
 output_data = "Output will appear here."
 
-@router.post("/input")
-def read_input(request: Request):
-    key = request.query_params.get("key")
-    # create_output(key) 
-    output_data = key
-    return {"input": key}
+# @router.post("/input")
+# def read_input(request: Request):
+#     key = request.query_params.get("key")
+#     output_data = key
+#     return {"input": key}
 
+logger = logging.getLogger("router")
+logger.setLevel(logging.DEBUG)
 
 
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    clients.append(websocket)
+    # clients.append(websocket)
     global output_data
     while True:
         try:
-            
-            await websocket.send_text(output_data)
-            output_data = await websocket.receive_text()
-            print("received: ",output_data)
-            # Send the new data to the client
-            # Your logic to generate or retrieve new data
-            
-        except WebSocketDisconnect:
-            clients.remove(websocket)
+            logger.error(str("received: " + output_data))
+            await websocket.send_text(str(output_data))
+            output_data= await websocket.receive_text()
+        except :
+            # clients.remove(websocket)
             break
--- frontend/src/components/History.vue
+++ frontend/src/components/History.vue
@@ -1,55 +1,48 @@
 <template>
   <section>
     <div>
-      <div >{{ output }}</div>
+      <div :key="output">{{ output }}</div>
     </div>
   </section>
 </template>
 
 <script>
-// import VueNativeSock from 'vue-native-websocket';
-
-//var ws = new WebSocket("ws://localhost:9000/ws");
 export default {
   data() {
     return {
       output: 'Backend not yet connected, please wait...',
+      connection: null // Store the WebSocket connection object
     };
   },
-  created: function() {
-    console.log("History starting connection to WebSocket Server")
-    this.connection = new WebSocket("ws://localhost:9000/ws")
+  mounted() {
+    console.log("History starting connection to WebSocket Server");
+    this.connection = new WebSocket("ws://localhost:9000/ws");
 
     this.connection.onmessage = (event) => {
-      // console.log(event);
       console.log("Event has been received: ", event);
       this.output = event.data;
-    }
+      this.$forceUpdate();
+    };
 
     this.connection.onopen = function(event) {
-      console.log(event)
-      console.log("History successfully connected to the websocket server...")
+      console.log("History successfully connected to the websocket server...");
+    };
+
+    // Optionally, handle WebSocket errors and close events
+    this.connection.onerror = function(error) {
+      console.error("WebSocket error:", error);
+    };
+
+    this.connection.onclose = function(event) {
+      console.log("WebSocket connection closed:", event);
+    };
+  },
+  beforeDestroy() {
+    // Close the WebSocket connection when the component is destroyed
+    if (this.connection) {
+      this.connection.close();
     }
   }
-
-    
-}
-//   created() {
-//     this.$options.sockets.onmessage = (event) => {
-//       const data = JSON.parse(event.data);
-//       if (data.hasOwnProperty('output')) {
-//         this.output = data.output;
-//       }
-//     };
-//   },
-//   mounted() {
-//     this.connectWebSocket();
-//   },
-//   methods: {
-//     connectWebSocket() {
-//       this.$options.sockets.connect('ws://localhost:9000/ws');
-//     }
-//   }
 };
 </script>
 
