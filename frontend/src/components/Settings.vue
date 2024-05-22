b43f88ea4d6a2f39ecb8d8a1313f93501cbf2ef8
Ali Rajabi <ali.rajabi@stud.th-deg.de>
2024-05-22 17:20:42 +0200

    implementation of dropdown list

--- frontend/src/components/Settings.vue
+++ frontend/src/components/Settings.vue
@@ -17,7 +17,17 @@
                         <table class = "table table-striped table-hover table-bordered">
                             <thead>
                                 <tr>
-                                    <th scope = "col">Reflektor: </th>
+                                    <th scope = "col">
+                                        <div class="dropdown">
+                                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" 
+                                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Reflektor</button>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 1')">UKW-B</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 2')">UKW-C</a>
+                                            </div>
+                                        </div>
+                                    </th>
+
                                     <th scope = "col">Rotor 1</th>
                                     <th scope = "col">Rotor 2</th>
                                     <th scope = "col">Rotor 3</th>
@@ -25,30 +35,89 @@
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td>Rotor</td>
-                                    <td>Rotor 1</td>
-                                    <td>Rotor 2</td>
-                                    <td>Rotor 3</td>
+                                    <td>Rotor:</td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" 
+                                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">I</button>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 1.1')">I</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 1.2')">II</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 1.3')">III</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 1.4')">IV</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 1.5')">V</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" 
+                                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">I</button>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 2.1')">I</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 2.2')">II</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 2.3')">III</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 2.4')">IV</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 2.5')">V</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" 
+                                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">I</button>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 3.1')">I</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 3.2')">II</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 3.3')">III</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 3.4')">IV</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 3.5')">V</a>
+                                            </div>
+                                        </div>
+                                    </td>
                                 </tr>
                                 <tr>
-                                    <td>Ausgangsposition</td>
-                                    <td>Ausgangsposition 1</td>
-                                    <td>Ausgangsposition 2</td>
-                                    <td>Ausgangsposition 3</td>
+                                    <td>Ausgangsposition:</td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" 
+                                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-size: smaller;">
+                                                {{ selectedPosition }}
+                                            </button>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="height: auto; max-height: 100px; overflow-y: auto; top: -150%; width: 8%;">
+                                                <a class="dropdown-item" v-for="letter in alphabet" :key="letter" @click="selectPostion(letter)">{{ letter }}</a>
+                                                
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td><div class="dropdown">
+                                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" 
+                                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">A</button>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Position_A_Rotor2')">A</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Position_B_Rotor2')">B</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Position_C_Rotor2')">C</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Position_D_Rotor2')">D</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Position_E_Rotor2')">E</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" 
+                                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">A</button>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Position_A_Rotor3')">A</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Position_B_Rotor3')">B</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Position_C_Rotor3')">C</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Position_D_Rotor3')">D</a>
+                                                <a class="dropdown-item" href="#" @click="selectRotor('Position_E_Rotor3')">E</a>
+                                            </div>
+                                        </div>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
-                        <!--Dropdown-Feld für die Rotoren Auswahl-->
-                        <div class="dropdown">
-                            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Rotoren auswählen
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 1')">Rotor 1</a>
-                                <a class="dropdown-item" href="#" @click="selectRotor('Rotor 2')">Rotor 2</a>
-                                <a class="dropdown-item" href="#" @click="selectRotor('Reflektor')">Reflektor</a>
-                            </div>
-                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -61,12 +130,28 @@
 </template>
 
 <script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            selectedPosition: 'A',
+        };
+    },
+    methods: {
+        selectPosition(position) {
+            this.selectedPosition = position;
+            console.log('Ausgewählte Ausgangsposition:', position);
+        }
+    }
+};
 </script>
 
 <style>
     #SettingsModal .modal-dialog{
         color: black;
-        max-width: 80%;
+        max-width: 50%;
+        height: 80%;
     }
 
     .table thead th{
@@ -79,4 +164,5 @@
     .modal-body{
         overflow-x: auto; 
     }
+    
 </style>
