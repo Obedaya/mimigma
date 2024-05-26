724438e8eac024ccdced0147e16f26f22f26c11f
Dorian Sanders <dorian.sanders@stud.th-deg.de>
2024-05-26 21:44:39 +0200

    Modified cypress keyboard click test

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -4,3 +4,12 @@ Cypress.Commands.add('login', (username, password) => {
   cy.get('input[id=passwort]').type(password);
   cy.get('button[type=submit]').click();
 });
+
+Cypress.Commands.add('clickKeyAndWait', (key) => {
+  cy.intercept('POST', `/keyboard?key=${key}`).as(`keyPress${key}`);
+  cy.get('.lower .key').contains(key).click();
+  cy.wait(`@keyPress${key}`, { timeout: 10000 }).then((interception) => {
+    cy.log(`Intercepted URL for ${key}: ${interception.request.url}`);
+    expect(interception.request.url).to.include(`key=${key}`);
+  });
+});
