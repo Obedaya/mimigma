ca776fb29b78e4ca50b5744bb8f4420d27212bf5
lg06087 <laura.gnther@stud.th-deg.de>
2024-05-28 05:43:41 +0200

    belongs to the previous commit

--- backend/app/routes/login.py
+++ backend/app/routes/login.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Request, HTTPException, status
+from fastapi import APIRouter, HTTPException, status
 from ..database import SessionLocal
 from ..models import User
 from ..utils import hash_password
 
 router = APIRouter()
 
-# Stephane : Endpoint to check user Login
+# Endpoint to check user Login
 @router.post("/login", tags=["User Management"])
 async def login(username: str, password: str):
     db = SessionLocal()
@@ -13,9 +13,14 @@ async def login(username: str, password: str):
         hashed_password = hash_password(password)
         user = db.query(User).filter(User.username == username, User.password == hashed_password).first()
         if user:
-            return {"message": "Login successful"}
+            return {
+                "message": "Login successful",
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                }
+            }
         else:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username or password")
     finally:
         db.close()
-
