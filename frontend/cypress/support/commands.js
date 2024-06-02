01f4382d79fa4c5dc1e5bd431ebb841d8e1eb6c0
Simon Holzner <simon.holzner@stud.th-deg.de>
2024-06-02 18:03:44 +0200

    Fixed Cypress Tests

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -1,4 +1,14 @@
 Cypress.Commands.add('login', (username, password) => {
+  cy.intercept({
+            method: '*', // You can also use '*' to match all methods if needed
+            url: /^http:\/\/localhost:([0-9]+)\/(.*)$/
+        }, (req) => {
+
+            req.url = req.url.replace('localhost', 'backend');
+
+            // Continue with the modified request
+            req.continue();
+        }).as('dynamicRedirect');
   cy.visit('/');
   cy.get('input[id=username]').type(username);
   cy.get('input[id=passwort]').type(password);
