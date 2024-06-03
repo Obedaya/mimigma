4da953341a27816ffbf2324bf24c9866cda18f09
pk30686 <paul.kronner@stud.th-deg.de>
2024-06-03 23:45:33 +0200

    Added Reflector

--- backend/app/enigma/reflector.py
+++ backend/app/enigma/reflector.py
@@ -1,5 +1,19 @@
-class Reflector:
-    # TODO: Extract Reflector setting from db and save it as a variable
+import string
+from reflector_config import UKW_A, UKW_B, UKW_C, UKW_N
 
-    def __init__(self):
-        pass
+class Reflector:
+    def __init__(self, variant):
+        self.variant = variant
+
+    # Expects A B C or N for the respective Enigma (reflector) variants 
+    # A B or C for Enigma I or M3 and N for Enigma Nord
+    def encrypt(self, input):       
+        if self.variant in ["A","B","C","N"] and input in list(string.ascii_uppercase):
+             return(eval("UKW_" + self.variant + "[" + "input" +"]"))
+        else:
+            raise Exception("Invalid input.")
+    
+    # Example usage:
+    # from reflector import Reflector
+    # r = Reflector("A")    # "A" is Enigma the Enigma Version
+    # r.encrypt("B")        # "B" is the letter to be 'encrypted'
