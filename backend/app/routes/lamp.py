9ace1c907380c1fc3abb313062536c48597ee56b
lg06087 <laura.gnther@stud.th-deg.de>
2024-06-09 13:24:19 +0200

    start enigma encryption

--- backend/app/routes/lamp.py
+++ backend/app/routes/lamp.py
@@ -1,49 +1,32 @@
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
-# from app.enigma.enigma import Enigma
+from fastapi import APIRouter, HTTPException, Depends, Query
 from sqlalchemy.orm import Session
-from ..database import SessionLocal
+from ..database import get_db, SessionLocal
 from ..models import Key
+from ..enigma.enigma import Enigma, get_rotor_settings_from_db
 
 router = APIRouter()
 
-class KeyInput(BaseModel):
-    key: str
-
-# example
-# enigma_machine = Enigma(
-#     rotors=['I', 'II', 'III'],
-#     reflector='B',
-#     ring_settings=[1, 1, 1],
-#     plugboard_settings={'A': 'B', 'C': 'D'}
-# )
-
-last_encrypted_key = None
-
-# dummy caesar
-def dummy_encrypt(key: str) -> str:
-    if not key.isalpha() or len(key) != 1:
-        raise ValueError("Invalid key")
-    return chr((ord(key.upper()) - 65 + 3) % 26 + 65)
+user_id = 4
 
 @router.get("/lamp", tags=["Lamp"])
 def get_encrypted_key():
     db = SessionLocal()
     try:
-        # Logic to get the lamp status for the last pressed key
-        current_key = db.query(Key).first()
+        current_key = db.query(Key).first()       
         current_key_value = current_key.key
+        print(f"Current key value from database: {current_key_value}")
     finally:
         db.close()
     try:
-        # dummy 
-        encrypted_key = dummy_encrypt(current_key_value)
-        print(encrypted_key)
+        machine_type, rotors, rotor_positions, ring_positions = get_rotor_settings_from_db(user_id, db)
+        reflector_type = "B"  # This can also be dynamic if stored in the database
+        print(f"Machine type: {machine_type}, Rotors: {rotors}, Rotor positions: {rotor_positions}, Ring positions: {ring_positions}")
         
-        # enigma
-        # encrypted_key = enigma_machine.encrypt(input.key.upper())
-        # last_encrypted_key = encrypted_key
-        
+        enigma_machine = Enigma(machine_type, rotors, rotor_positions, ring_positions, reflector_type)
+        encrypted_key = enigma_machine.encrypt_message(input.key.upper())
+        print(f"Encrypted key: {encrypted_key}")
+
         return {"encrypted_key": encrypted_key}  # Ensure this format
     except Exception as e:
+        print(f"Exception: {str(e)}")
         raise HTTPException(status_code=400, detail=str(e))
