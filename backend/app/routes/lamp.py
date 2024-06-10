7b7640507188c335bc00bf937990b3804f01b21c
lg06087 <laura.gnther@stud.th-deg.de>
2024-06-10 13:11:04 +0200

    fixed issue in lamp.py

--- backend/app/routes/lamp.py
+++ backend/app/routes/lamp.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, HTTPException, Depends, Query
 from sqlalchemy.orm import Session
 from ..database import get_db, SessionLocal
 from ..models import Key
-from ..enigma.enigma import Enigma, get_rotor_settings_from_db
+from ..enigma.enigma import Enigma
+from ..enigma.rotor import get_rotor_settings_from_db
 
 router = APIRouter()
 
@@ -11,19 +12,21 @@ user_id = 4
 @router.get("/lamp", tags=["Lamp"])
 def get_encrypted_key():
     db = SessionLocal()
+    # try:
+    #     current_key = db.query(Key).first()
+    #     current_key_value = current_key.key
+    #     print(f"Current key value from database: {current_key_value}")
+    # finally:
+    #     db.close()
     try:
-        current_key = db.query(Key).first()       
+        current_key = db.query(Key).first()
         current_key_value = current_key.key
-        print(f"Current key value from database: {current_key_value}")
-    finally:
-        db.close()
-    try:
         machine_type, rotors, rotor_positions, ring_positions = get_rotor_settings_from_db(user_id, db)
         reflector_type = "B"  # This can also be dynamic if stored in the database
-        print(f"Machine type: {machine_type}, Rotors: {rotors}, Rotor positions: {rotor_positions}, Ring positions: {ring_positions}")
-        
+        print(f"Machine type: {machine_type}, Rotors: {rotors}, Rotor positions: {rotor_positions}, Ring positions: {ring_positions}, Reflector type: {reflector_type}")
+
         enigma_machine = Enigma(machine_type, rotors, rotor_positions, ring_positions, reflector_type)
-        encrypted_key = enigma_machine.encrypt_message(input.key.upper())
+        encrypted_key = enigma_machine.encrypt_message(current_key_value.upper())
         print(f"Encrypted key: {encrypted_key}")
 
         return {"encrypted_key": encrypted_key}  # Ensure this format
