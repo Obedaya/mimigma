f897ac2a4a52f2b6a022692dd5ba8a5e52690b81
lg06087 <laura.gnther@stud.th-deg.de>
2024-06-30 14:09:10 +0200

    removed unnecessary text

--- frontend/src/components/Usersettings.vue
+++ frontend/src/components/Usersettings.vue
@@ -14,7 +14,6 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        Eingeloggter Benutzer:
                         {{ auth.user.username }}
                     </div>
                     <div class="modal-footer">
